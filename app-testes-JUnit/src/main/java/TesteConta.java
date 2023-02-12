4543807e291e503b95a051a84a35af14b515acb9
theJoseAlan <salesalan854@gmail.com>
2023-02-11 21:30:40 -0300

    commit inicial

---
+++ app-testes-JUnit/src/main/java/TesteConta.java
@@ -0,0 +1,27 @@
+public class TesteConta {
+    public static void main(String[] args) {
+
+
+        //Depositar com conta ativa
+        Conta conta = new Conta();
+
+        conta.ativar();
+        conta.depositar(100);
+
+        System.out.println();
+        System.out.println("Deve depositar com conta ativa");
+        System.out.println("Saldo conta 1: "+conta.getSaldo());
+
+        //Depositar com conta inativa
+        Conta conta2 = new Conta();
+
+        conta2.inativar();
+        conta2.depositar(100);
+
+        System.out.println();
+        System.out.println("Não deve depositar com conta inativa");
+        System.out.println("Saldo conta 2: "+conta2.getSaldo());
+
+
+    }
+}
---
+++ app-testes-JUnit/src/test/java/InicialTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InicialTest {
+
+    @Test
+    public void verificarJUnit(){
+        Assertions.assertTrue(true);
+    }
+
+}
