a851a3b4ee2653b7875fef083db2dc0ebdd21f9e
theJoseAlan <salesalan854@gmail.com>
2023-02-28 09:43:43 -0300

    Classe usuário

--- JUnit - ValdirCezar/src/main/java/br/com/dicasdeumdev/api/ApiApplication.java
+++ JUnit - ValdirCezar/src/main/java/br/com/dicasdeumdev/api/ApiApplication.java
@@ -1,5 +1,6 @@
-package com.example.demo;
+package br.com.dicasdeumdev.api;
 
+import br.com.dicasdeumdev.api.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiApplication.class, args);
+
+		User user = new User(1, "Alan", "salesaaln@gmail.com", "04032000");
+
 	}
 
 }
---
+++ JUnit - ValdirCezar/src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -0,0 +1,27 @@
+package br.com.dicasdeumdev.api.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+}
