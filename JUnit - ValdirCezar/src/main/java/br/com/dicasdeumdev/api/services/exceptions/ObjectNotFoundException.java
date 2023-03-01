47fd05f1645cedffdebec433fcb555d59b1734d8
theJoseAlan <salesalan854@gmail.com>
2023-03-01 10:26:11 -0300

    tratamento de exceção para objeto não encontrado

---
+++ JUnit - ValdirCezar/src/main/java/br/com/dicasdeumdev/api/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.dicasdeumdev.api.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String message){
+        super(message);
+    }
+
+}
