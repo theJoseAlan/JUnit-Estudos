5c76337df08aec3a44825b39dcbfe44c2f51bbdf
theJoseAlan <salesalan854@gmail.com>
2023-03-01 10:58:33 -0300

    Model mapper e DTO

--- JUnit - ValdirCezar/src/main/java/br/com/dicasdeumdev/api/resources/UserResources.java
+++ JUnit - ValdirCezar/src/main/java/br/com/dicasdeumdev/api/resources/UserResources.java
@@ -1,7 +1,8 @@
 package br.com.dicasdeumdev.api.resources;
 
-import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResources {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 
 }
