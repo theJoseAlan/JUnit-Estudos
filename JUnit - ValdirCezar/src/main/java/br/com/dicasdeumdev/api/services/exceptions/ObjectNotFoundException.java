7ac169daeb3b2c5f943389026093260be2856106
theJoseAlan <salesalan854@gmail.com>
2023-03-06 09:24:14 -0300

    Restart projetc JUnit from Api

--- JUnit - ValdirCezar/src/main/java/br/com/dicasdeumdev/api/services/exceptions/ObjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.com.dicasdeumdev.api.services.exceptions;
-
-public class ObjectNotFoundException extends RuntimeException{
-
-    public ObjectNotFoundException(String message){
-        super(message);
-    }
-
-}
