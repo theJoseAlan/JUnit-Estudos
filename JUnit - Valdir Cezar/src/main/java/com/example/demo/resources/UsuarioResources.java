e290cb2766f92f7334298e5ac78009db21106632
theJoseAlan <salesalan854@gmail.com>
2023-03-06 10:45:37 -0300

    Usuario configs

--- JUnit - Valdir Cezar/src/main/java/com/example/demo/resources/UsuarioResources.java
+++ JUnit - Valdir Cezar/src/main/java/com/example/demo/resources/UsuarioResources.java
@@ -1,6 +1,8 @@
 package com.example.demo.resources;
 
 import com.example.demo.domain.Usuario;
+import com.example.demo.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/usuario")
 public class UsuarioResources {
 
+    @Autowired
+    private UsuarioService service;
+
     @GetMapping(value = "{id}")
     public ResponseEntity<Usuario> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(new Usuario(1, "Alan", "alan@gmail.com", "senha123 "));
+        return ResponseEntity.ok().body(service.findById(id));
 
     }
 
---
+++ JUnit - Valdir Cezar/src/main/java/com/example/demo/services/UsuarioService.java
@@ -0,0 +1,9 @@
+package com.example.demo.services;
+
+import com.example.demo.domain.Usuario;
+
+public interface UsuarioService {
+
+    Usuario findById(Integer id);
+
+}
