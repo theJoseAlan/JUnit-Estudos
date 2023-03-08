1451024e7991e10097564f73d44ab5928d3a1b5c
theJoseAlan <salesalan854@gmail.com>
2023-03-08 09:19:42 -0300

    Create UsuarioDTO.java

---
+++ JUnit - Valdir Cezar/src/main/java/com/example/demo/domain/dto/UsuarioDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.domain.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDTO {
+
+    private Integer id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+}
