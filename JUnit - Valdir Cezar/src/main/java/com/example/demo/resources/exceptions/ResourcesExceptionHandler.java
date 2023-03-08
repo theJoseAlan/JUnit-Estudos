bfbc0bc3aa192ef6f33a529fdbf283f3035fb9f7
theJoseAlan <salesalan854@gmail.com>
2023-03-08 09:04:36 -0300

    Update ResourcesExceptionHandler.java

--- JUnit - Valdir Cezar/src/main/java/com/example/demo/resources/exceptions/ResourcesExceptionHandler.java
+++ JUnit - Valdir Cezar/src/main/java/com/example/demo/resources/exceptions/ResourcesExceptionHandler.java
@@ -18,6 +18,6 @@ public class ResourcesExceptionHandler {
                 ex.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-    } 
+    }
 
 }
