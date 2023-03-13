7cb96a48c48dc04205e16245baa7390b59c5be0f
theJoseAlan <salesalan854@gmail.com>
2023-03-13 09:19:50 -0300

    Teste de FindById

--- JUnit - Valdir Cezar/src/test/java/com/example/demo/services/mpl/UsuarioServicemplTest.java
+++ JUnit - Valdir Cezar/src/test/java/com/example/demo/services/mpl/UsuarioServicemplTest.java
@@ -3,24 +3,29 @@ package com.example.demo.services.mpl;
 import com.example.demo.domain.Usuario;
 import com.example.demo.domain.dto.UsuarioDTO;
 import com.example.demo.respositories.UsuarioRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UsuarioServicemplTest {
 
 
-    public static final int ID = 1;
-    public static final String ALAN_SALES = "Alan Sales";
+    public static final Integer ID = 1;
+    public static final String NOME = "Alan Sales";
     public static final String EMAIL = "alansales@gmail.com";
     public static final String SENHA = "1234";
     @InjectMocks //Indicando que precisa de uma instância real para testar os métodos
@@ -40,13 +45,44 @@ class UsuarioServicemplTest {
 
     @BeforeEach
     void setUp(){
-        //Inicia os Mocks. oO this faz referência a essa classe
+        //Inicia os Mocks. O "this" referencia essa classe
         MockitoAnnotations.openMocks(this);
+        startUser(); //Sempre que iniciar a classe, ele starta os mocks e inicia os usuários
 
     }
 
     @Test
+    @DisplayName("FindByID deve retornar uma instância de usuário")
     void findById() {
+        /*Quando repository.findById for chamado por qualquer numero inteiro (anyInt)
+        então retorne o optional user*/
+        when(repository.findById(anyInt())).thenReturn(optionalUsuario);
+
+        Usuario response = service.findById(ID);
+
+        //Assegure que o response não vai ser nulo
+        Assertions.assertNotNull(response);
+
+        //Assegure que esses dois argumentos são iguais.
+        //O primeiro é o que espero receber, o segundo é o que o método retornará
+        assertEquals(Usuario.class, response.getClass());
+
+        //Testando os atributos esperados e retornados
+
+        //Assegure que o ID do incio da classe
+        //vai ser igual ao ID do response
+        //ID esperado -> 1
+        assertEquals(ID, response.getId());
+
+        //Assegure que o NOME do incio da classe
+        //vai ser igual ao NOME do response
+        assertEquals(NOME, response.getNome());
+
+        //Assegure que o EMAIL do incio da classe
+        //vai ser igual ao EMAIL do response
+        assertEquals(EMAIL, response.getEmail());
+
+
     }
 
     @Test
@@ -71,8 +107,8 @@ class UsuarioServicemplTest {
 
 
     private void startUser(){
-        usuario = new Usuario(ID, ALAN_SALES, EMAIL, SENHA);
-        usuarioDTO = new UsuarioDTO(ID, ALAN_SALES, EMAIL, SENHA);
-        optionalUsuario = Optional.of(new Usuario(ID, ALAN_SALES, EMAIL, SENHA));
+        usuario = new Usuario(ID, NOME, EMAIL, SENHA);
+        usuarioDTO = new UsuarioDTO(ID, NOME, EMAIL, SENHA);
+        optionalUsuario = Optional.of(new Usuario(ID, NOME, EMAIL, SENHA));
     }
 }
